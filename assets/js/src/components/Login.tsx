b98313c74307e325cab76cb5aac23c53882bae70
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2024-05-15 22:17:21 -0600

    login and register forms

--- assets/js/src/components/Login.tsx
+++ assets/js/src/components/Login.tsx
@@ -1,50 +1,98 @@
-import React, { useState } from "react";
+import React from "react";
 import { useLocation } from "wouter";
 import { loginUser } from "../api";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Card, CardContent, CardHeader, CardTitle } from "./Card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./Form";
+import { Input } from "./Input";
+import { Button } from "./Button";
 
 export function Login() {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   const [location, setLocation] = useLocation();
+
+  const formSchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+  });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
   const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: loginUser,
-    onSuccess: (data) => {
-      queryClient.setQueryData(["currentUser"], data);
-      setLocation("/");
-    },
+    onSuccess: (data) => queryClient.setQueryData(["currentUser"], data),
   });
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email:
-          <input
-            type="text"
-            value={email}
-            onChange={(v) => setEmail(v?.target?.value)}
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(v) => setPassword(v?.target?.value)}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+    <>
+      <div className="container w-1/2">
+        <Card>
+          <CardHeader>
+            <CardTitle>Register</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                className="space-y-8"
+                onSubmit={form.handleSubmit(
+                  async (data) => await handleSubmit(data),
+                )}
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter your email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                ></FormField>
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input type="password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                ></FormField>
+                <Button
+                  className="w-full"
+                  onClick={form.handleSubmit(handleSubmit)}
+                >
+                  Login
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 
-  function handleSubmit(e: any) {
-    e.preventDefault();
+  async function handleSubmit({ email, password }: z.infer<typeof formSchema>) {
     try {
-      mutation.mutate({ email, password });
+      await mutation.mutateAsync({ email, password });
+      setLocation("/");
     } catch (error) {
       console.error(error);
     }
--- assets/js/src/components/Register.tsx
+++ assets/js/src/components/Register.tsx
@@ -2,11 +2,38 @@ import React, { useState } from "react";
 import { useLocation } from "wouter";
 import { registerUser } from "../api";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Card, CardContent, CardHeader, CardTitle } from "./Card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./Form";
+import { Input } from "./Input";
+import { Button } from "./Button";
 
 export function Register() {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   const [location, setLocation] = useLocation();
+
+  const formSchema = z
+    .object({
+      email: z.string().email(),
+      password: z.string(),
+      confirm: z.string(),
+    })
+    .refine((data) => data.password === data.confirm, {
+      message: "Passwords don't match",
+      path: ["confirm"],
+    });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
   const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: registerUser,
@@ -14,29 +41,74 @@ export function Register() {
   });
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input
-          type="text"
-          value={email}
-          onChange={(v) => setEmail(v?.target?.value)}
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(v) => setPassword(v?.target?.value)}
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+    <>
+      <div className="container w-1/2">
+        <Card>
+          <CardHeader>
+            <CardTitle>Register</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                className="space-y-8"
+                onSubmit={form.handleSubmit(
+                  async (data) => await handleSubmit(data),
+                )}
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Enter your email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                ></FormField>
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input type="password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                ></FormField>
+                <FormField
+                  control={form.control}
+                  name="confirm"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Confirm Password</FormLabel>
+                      <FormControl>
+                        <Input type="password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                ></FormField>
+                <Button
+                  className="w-full"
+                  onClick={form.handleSubmit(handleSubmit)}
+                >
+                  Register
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+    </>
   );
 
-  async function handleSubmit(e: any) {
-    e.preventDefault();
+  async function handleSubmit({ email, password }: z.infer<typeof formSchema>) {
     try {
       await mutation.mutateAsync({ email, password });
       setLocation("/");
